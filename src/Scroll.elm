de76fc3675bfb4a456d50ec85e2d418fc99c3bf6
lue-bird <lue.the.bird@gmail.com>
2022-07-05 18:35:13 +0200

    `Scroll` alternatives section (in readme → `Scroll`)

--- src/Scroll.elm
+++ src/Scroll.elm
@@ -24,6 +24,8 @@ module Scroll exposing
 
 [`Scroll`](#Scroll) can even focus a gap [`Down` or `Up`](https://dark.elm.dmy.fr/packages/lue-bird/elm-linear-direction/latest/) from every item.
 
+Not what you were looking for? Check out [alternatives](#alternatives).
+
 @docs Scroll, FocusGap
 
 
@@ -69,6 +71,31 @@ module Scroll exposing
 ## type-level
 
 @docs focusGapAdapt
+
+
+## alternatives
+
+  - [zwilias/elm-holey-zipper](https://package.elm-lang.org/packages/zwilias/elm-holey-zipper/latest).
+    unsafe; a bit cluttered; no `map (Location -> ...)`, `focusDrag`, `toNonEmptyList`, `sideAlter` (so no squeezing in multiple items, ...)
+  - [turboMaCk/non-empty-list-alias: `List.NonEmpty.Zipper`](https://dark.elm.dmy.fr/packages/turboMaCk/non-empty-list-alias/latest/List-NonEmpty-Zipper)
+    complete; cluttered (for example `update` & `map`); some unintuitive names
+  - [miyamoen/select-list](https://dark.elm.dmy.fr/packages/miyamoen/select-list/latest/SelectList)
+    complete; a bit cluttered; no `focusWhere`
+  - [yotamDvir/elm-pivot](https://dark.elm.dmy.fr/packages/yotamDvir/elm-pivot/latest/)
+    complete; cluttered; no `map (Location -> ...)`
+  - [STTR13/ziplist](https://dark.elm.dmy.fr/packages/STTR13/ziplist/latest/)
+    navigation works; a bit cluttered; no `map (Location -> ...)`, `sideAlter` (so no squeezing in multiple items, ...)
+  - [wernerdegroot/listzipper](https://dark.elm.dmy.fr/packages/wernerdegroot/listzipper/latest/List-Zipper)
+    navigation works; no `focusDrag`, `map (Location -> ...)`, `toNonEmptyList`
+  - [alexanderkiel/list-selection](https://dark.elm.dmy.fr/packages/alexanderkiel/list-selection/latest/List-Selection)
+    & [NoRedInk/list-selection](https://dark.elm.dmy.fr/packages/NoRedInk/list-selection/latest/List-Selection)
+    very incomplete, impossible to extract focused item safely; no navigation, insertion, `side`, ...
+  - [jjant/elm-comonad-zipper](https://dark.elm.dmy.fr/packages/jjant/elm-comonad-zipper/latest/)
+    incomplete; no `focusDrag`, `toNonEmptyList`, `sideAlter` (so no squeezing in multiple items, ...)
+  - [guid75/ziplist](https://dark.elm.dmy.fr/packages/guid75/ziplist/latest/)
+    extremely incomplete
+  - [arowM/elm-reference: `Reference.List`](https://dark.elm.dmy.fr/packages/arowM/elm-reference/latest/Reference-List)
+    only `overList`, sides impossible to access & alter
 
 -}
 
